6909c74b8c53a376ae8827c6dd10612bf42c55fd
mngv7 <165127020+mngv7@users.noreply.github.com>
2024-04-28 17:33:09 +1000

    Completed section 1.1

--- missionA2S1.m
+++ missionA2S1.m
@@ -9,3 +9,30 @@ clear all; close all;
 load DataA2 audioMultiplexNoisy fs sid;
 
 % Begin writing your MATLAB solution below this line.
+
+% 1.1 Plot the time and frequency domain of audioMultiplexNoisy
+
+samples = length(audioMultiplexNoisy);
+t = linspace(0, samples / fs, samples + 1);
+t(end) = [];
+
+% Plot original audio signal
+figure;
+subplot(2, 1, 1);
+plot(t, audioMultiplexNoisy, 'k');
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Multiplexed Audio Signal (Time Domain)');
+
+samples = length(audioMultiplexNoisy);
+f = linspace(-fs/2, fs/2, samples+1);
+f(end) = [];
+
+AudioMultiplexNoisy =  fftshift(fft(audioMultiplexNoisy)) / fs;
+
+% Plot magnitude spectrum
+subplot(2, 1, 2);
+plot(f, AudioMultiplexNoisy, 'k');
+title('Multiplexed Audio Signal (Frequency Domain)');
+xlabel('Frequency [Hz]');
+ylabel('Magnitude');
