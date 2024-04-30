2ba2ae824b9d6207c237d20abbb69703eefc96bf
rrndomingo2003 <168379768+rrndomingo2003@users.noreply.github.com>
2024-04-30 19:09:18 +1000

    Revised and added functions to previous code

--- missionA2S1.m
+++ missionA2S1.m
@@ -10,12 +10,12 @@ load DataA2 audioMultiplexNoisy fs sid;
 
 % Begin writing your MATLAB solution below this line.
 
-% 1.1 Plot the time and frequency domain of audioMultiplexNoisy
+%% 1.1 Plot the time and frequency domain of audioMultiplexNoisy
 
 % Generate appropriate time vector.
 samples = length(audioMultiplexNoisy);
-t = linspace(0, samples / fs, samples + 1);
-t(end) = [];
+t = timevec(0, samples / fs, samples);
+
 
 % Plot the time domain of audioMultiplexNoisy.
 figure;
@@ -27,31 +27,32 @@ title('Multiplexed Audio Signal (Time Domain)');
 
 % Generate appropriate frequency vector.
 samples = length(audioMultiplexNoisy);
-f = linspace(-fs/2, fs/2, samples+1);
-f(end) = [];
+f = freqvec(fs, samples);
+
 
 % Convert audioMultiplexNoisy to frequency domain using Fourier transform.
-AudioMultiplexNoisy =  fftshift(fft(audioMultiplexNoisy)) / fs;
+AudioMultiplexNoisy =  ft(audioMultiplexNoisy, fs);
 
 % Plot the frequency domain of audioMultiplexNoisy.
 subplot(2, 1, 2);
-plot(f, AudioMultiplexNoisy, 'k');
+plot(f, abs(AudioMultiplexNoisy), 'k');
 title('Multiplexed Audio Signal (Frequency Domain)');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 
-% 1.2 Demodulate the signals in the frequency domain, listen, and plot.
+%% 1.2 Demodulate the signals in the frequency domain, listen, and plot.
 
 % Signals probably need filtering?
 
 % Frequencies to demodulate:
+% 8.05  kHz
 % 24.33 kHz
 % 40.27 kHz
 % 56.17 kHz
 % 72.29 kHz
 
-% First signal centred at 24.33 kHz.
-carrierFrequency = 24330;
+% First signal centred at 8.05 kHz.
+carrierFrequency = 8050;
 
 carrierSignal = cos(2*pi*carrierFrequency*t);
 
@@ -59,7 +60,7 @@ firstSignalDemodulated = audioMultiplexNoisy .* carrierSignal;
 
 %sound(firstSignalDemodulated, fs);
 
-FirstSignalDemodulated =  fftshift(fft(firstSignalDemodulated)) / fs;
+FirstSignalDemodulated =  ft(firstSignalDemodulated, fs);
 
 figure;
 subplot(2, 1, 1);
@@ -69,13 +70,13 @@ ylabel('Amplitude');
 title('First Signal Demodulated (Time Domain)');
 
 subplot(2, 1, 2);
-plot(f, FirstSignalDemodulated, 'k');
+plot(f, abs(FirstSignalDemodulated), 'k');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 title('First Signal Demodulated (Frequency Domain)');
 
-% Second signal centred at 40.27 kHz
-carrierFrequency = 40270;
+%% Second signal centred at 24.33 kHz
+carrierFrequency = 24330;
 
 carrierSignal = cos(2*pi*carrierFrequency*t);
 
@@ -83,7 +84,7 @@ secondSignalDemodulated = audioMultiplexNoisy .* carrierSignal;
 
 %sound(secondSignalDemodulated, fs);
 
-SecondSignalDemodulated =  fftshift(fft(secondSignalDemodulated)) / fs;
+SecondSignalDemodulated =  ft(secondSignalDemodulated, fs);
 
 figure;
 subplot(2, 1, 1);
@@ -93,13 +94,13 @@ ylabel('Amplitude');
 title('Second Signal Demodulated (Time Domain)');
 
 subplot(2, 1, 2);
-plot(f, SecondSignalDemodulated, 'k');
+plot(f, abs(SecondSignalDemodulated), 'k');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 title('Second Signal Demodulated (Frequency Domain)');
 
-% Third signal centred at 56.17 kHz
-carrierFrequency = 56170;
+%% Third signal centred at 40.27 kHz
+carrierFrequency = 40270;
 
 carrierSignal = cos(2*pi*carrierFrequency*t);
 
@@ -107,7 +108,7 @@ thirdSignalDemodulated = audioMultiplexNoisy .* carrierSignal;
 
 %sound(thirdSignalDemodulated, fs);
 
-ThirdSignalDemodulated =  fftshift(fft(thirdSignalDemodulated)) / fs;
+ThirdSignalDemodulated =  ft(thirdSignalDemodulated, fs);
 
 figure;
 subplot(2, 1, 1);
@@ -117,13 +118,13 @@ ylabel('Amplitude');
 title('Third Signal Demodulated (Time Domain)');
 
 subplot(2, 1, 2);
-plot(f, ThirdSignalDemodulated, 'k');
+plot(f, abs(ThirdSignalDemodulated), 'k');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 title('Third Signal Demodulated (Frequency Domain)');
 
-% Fourth signal centred at 72.29 kHz
-carrierFrequency = 72290;
+%% Fourth signal centred at 56.17 kHz
+carrierFrequency = 56170;
 
 carrierSignal = cos(2*pi*carrierFrequency*t);
 
@@ -131,7 +132,7 @@ fourthSignalDemodulated = audioMultiplexNoisy .* carrierSignal;
 
 %sound(fourthSignalDemodulated, fs);
 
-FourthSignalDemodulated =  fftshift(fft(fourthSignalDemodulated)) / fs;
+FourthSignalDemodulated =  ft(fourthSignalDemodulated, fs);
 
 figure;
 subplot(2, 1, 1);
@@ -141,11 +142,34 @@ ylabel('Amplitude');
 title('Fourth Signal Demodulated (Time Domain)');
 
 subplot(2, 1, 2);
-plot(f, FourthSignalDemodulated, 'k');
+plot(f, abs(FourthSignalDemodulated), 'k');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 title('Fourth Signal Demodulated (Frequency Domain)');
 
+%% Fifth signal centred at 72.29 kHz
+carrierFrequency = 72290;
+
+carrierSignal = cos(2*pi*carrierFrequency*t);
+
+fifthSignalDemodulated = audioMultiplexNoisy .* carrierSignal;
+
+%sound(fifthSignalDemodulated, fs);
+
+FifthSignalDemodulated =  ft(fifthSignalDemodulated, fs);
+
+figure;
+subplot(2, 1, 1);
+plot(t, fifthSignalDemodulated, 'k');
+xlabel('Time (seconds)');
+ylabel('Amplitude');
+title('Fifth Signal Demodulated (Time Domain)');
+
+subplot(2, 1, 2);
+plot(f, abs(FifthSignalDemodulated), 'k');
+xlabel('Frequency [Hz]');
+ylabel('Amplitude');
+title('Fifth Signal Demodulated (Frequency Domain)');
 %% 1.3 Frequency and impulse response of the LTI system
 
 % Vout(t)/Vin(t) = h(t)
@@ -153,8 +177,9 @@ title('Fourth Signal Demodulated (Frequency Domain)');
 
 
 y = channel(sid, audioMultiplexNoisy, fs);
-Y = fftshift(fft(y)) / fs;
+Y = ft(y, fs);
 H_f = Y ./ AudioMultiplexNoisy; % check if this needs to be a different test signal
+
 
 figure;
 plot(f, abs(H_f), 'r', f, abs(AudioMultiplexNoisy), 'b');
@@ -166,7 +191,7 @@ grid on;
 
 %% 1.4 Reverse distortion
 AudioMultiplexReverse = Y ./ H_f;
-audioMultiplexReverse = ifft(ifftshift(AudioMultiplexReverse)) * fs;
+audioMultiplexReverse = ift(AudioMultiplexReverse, fs);
 
 % Demodulate signal again
 carrierFrequency = 24330;
@@ -174,8 +199,9 @@ carrierFrequency = 24330;
 carrierSignal = cos(2*pi*carrierFrequency*t);
 demodAudioMultiplexReverse = audioMultiplexReverse .* carrierSignal;
 
+%sound(demodAudioMultiplexReverse, fs);
 
-DemodAudioMultiplexReverse = fftshift(fft(demodAudioMultiplexReverse)) / fs;
+DemodAudioMultiplexReverse = ft(demodAudioMultiplexReverse, fs);
 
 % plot in time and frequency domains
 figure;
@@ -185,9 +211,104 @@ xlabel('Time (s)');
 ylabel('Amplitude');
 title('Demodulated multiplexed audio signal with reversed distortion (Time Domain)');
 subplot(2,1,2);
-plot(f, DemodAudioMultiplexReverse, 'r');
+plot(f, abs(DemodAudioMultiplexReverse), 'r');
 xlabel('Frequency [Hz]');
 ylabel('Amplitude');
 title('Demodulated multiplexed audio signal with reversed distortion (Frequency Domain)');
 
 
+
+
+%% helper functions
+% function definitions in matlab either need to be in their own file,
+% or can be in at the bottom of a script.
+% if you want to these functions outside this lab, feel free to 
+% move them into their own file, just make sure the filename is the same 
+% as the function name, ie timevec.m, freqvec.m
+
+
+function t=timevec(t0, t0_plus_T, n)
+% Creates time vector, where upper limit is non-inclusive
+%          t0 <= t < t0_plus_T
+%   It is the responsibility of the user to ensure that for the use-case
+%   that they want the lower limit included, and the upper-limit 
+%   not included.
+%
+%   Args:
+%   t0 = start time
+%   t0_plus_T = end time (t0 + T)
+%   n = number of samples
+
+    t = linspace(t0, t0_plus_T, n + 1);
+    t = t(1:end - 1);
+end
+
+
+function fourierTransform = ft(freq, n)
+% Applies complex Fourier transform with the assigned sampling frequency
+%
+%   Args:
+%   freq = frequency/signal input
+%   n = sample frequency in Hz
+
+    fourierTransform = fftshift(fft(freq)) / n;
+end
+
+function inversefourierTransform = ift(freq, n)
+% Applies inverse Fourier transform with the assigned sampling frequency
+%
+%   Args:
+%   freq = frequency/signal input
+%   n = sample frequency in Hz
+
+    inversefourierTransform = ifft(ifftshift(freq)) * n;
+end
+
+
+function f=freqvec(fs, n)
+% Creates frequency vector suitable for plotting magnitude/phase spectrum
+%
+%  This function emulates the np.fft.freqvec function from python, but will
+%  also make sure that the frequency vector has been shifted correctly, so
+%  that the first index is for the lowest frequency, highest index is for
+%  highest and that the middle frequency is DC.
+
+%  https://numpy.org/doc/stable/reference/generated/numpy.fft.fftfreq.html
+%  
+%  The frequency vector will be slightly different if the sequence is of 
+%  even length or odd length. The specifics of this has to do with 
+%  properties of the Discrete Fourier Transform (DTF), which is what the
+%  FFT algorigthm actually computes. Will likely cover
+%  this in your Digital signal processing class. The main thing to know is 
+%  that this function will create a frequency vector that will ensure the DC
+%  Component is at the correct location. For now, can just take our
+%  word for it, and know what the function does and what it will return.
+%
+% 
+%  For even length signals, our frequency vector will be of the form,
+%        -fs/2 <= f < fs/2
+%  For odd length signals, will be,
+%        -fs/ 2 < f < fs/2
+%
+%  Args:
+%  fs = sample frequency in Hz
+%  n = length of the time vector/number of samples
+
+
+    % if is an even sequence length, generating the frequency vector
+    % is just like doing it for our time vector in the timevec function
+    if mod(n, 2) == 0
+        f_str = sprintf('Generating freq vec\n [%.2f, %.2f)\n', -fs/2, fs/2);
+        disp(f_str);
+        % compute the frequency vector
+        f = linspace(-fs / 2, fs / 2, n + 1);
+        f = f(1:end - 1); 
+    % otherwise is of odd length
+    else        
+        f_str = sprintf('Generating freq vec\n (%.2f, %.2f)\n', ...
+            -(n -1)/2 * fs / n, (n -1)/2 * fs / n);
+        disp(f_str);
+        % compute the frequency vector
+        f = linspace(-(n -1)/ 2, (n -1) / 2, n) * fs / n;
+    end
+end
