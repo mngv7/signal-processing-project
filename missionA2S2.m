dcfed87f2219c849176e3f11b3a8b89a4f38d07a
rrndomingo2003 <168379768+rrndomingo2003@users.noreply.github.com>
2024-05-19 19:29:11 +1000

    Done

--- missionA2S2.m
+++ missionA2S2.m
@@ -1,92 +1,142 @@
-%%%% EGB242 Assignment 2, Section 3 %%
-% This file is a template for your MATLAB solution to Section 3.
+%% EGB242 Assignment 2, Section 2 %%
+% This file is a template for your MATLAB solution to Section 2.
 %
 % Before starting to write code, generate your data with the ??? as
 % described in the assignment task.
 
 %% Initialise workspace
-clear all; close all; clc;
-load DataA2 imagesReceived;
+clear all; close all;
 
-% Begin writing your MATLAB solution below this line.
-%% Question 3.1
-im1D = imagesReceived(1,:);
+%% 2.1
 
-% Converting a received pixel stream to an image matrix
-im2D = reshape(im1D, 480, 640) ;
-
-% Displaying an image in a figure
-figure;
-imshow (im2D);
-
-% Saving an image matrix as an image file 
-imwrite (im2D , 'InitialImage.png') ;
-
-%% Question 3.2
-samples = length(imagesReceived);
-f = 1000;
-T = 1/1000;
-% Creating the time vector
-t = timevec(0, T * samples, samples); 
-% Creating the Frequency vector
-fs = freqvec(f, samples); 
-
-im2D = im2D(1:samples);
-
-figure;
-plot(t, im2D);
-xlim([0,307]);
-ylim([-7,7]);
-xlabel('Time [s]');
-ylabel('Magnitdue');
-title('Received image signal in Time Domain');
-
-% IM2D = ft(im2D, fs);
-% plot(fs, abs(IM2D));
-% xlabel('Frequency [Hz]');
-% ylabel('Magnitude');
-% title('Received image signal in Frequency Domain');
-
-%% Question 3.3
+t2 = timevec(0, 20, 100000); % Time vector
 
 % Step input
-step_input = ones(size(t));
+step_input = ones(size(t2));
 
-% Define transfer function Filter 1
-Pnum1 = [1]; 
-Pden1 = [5.64e-5, 0.0167, 1]; 
-Passive1tf = tf(Pnum1, Pden1);
+% Step response
+step_response = 2*t2 + 4*exp(-0.5*t2) - 4;
 
-% Define transfer function Filter 2
-Pnum2 = [1.2e-3]; 
-Pden2 = [5.64e-5, 5.9e-3, 1]; 
-Passive2tf = tf(Pnum2, Pden2);
+% Plotting
+figure;
+subplot(2,1,1);
+plot(t2, step_input, 'b', 'LineWidth', 1.5); % Step input
+xlabel('Time (s)');
+ylabel('Voltage (V)');
+title('Step Input');
+grid on;
 
-% Define transfer function Filter 1
-Anum1 = [1.22e3]; 
-Aden1 = [1, 2.44e3,1.44e6];
-Active1tf = tf(Anum1, Aden1);
+subplot(2,1,2);
+plot(t2, step_response, 'r', 'LineWidth', 1.5); % Step response
+xlabel('Time (s)');
+ylabel('Radians (rad)');
+title('Step Response');
+grid on;
 
-% Define transfer function Filter 2
-Anum2 = [1]; 
-Aden2 = [1, 2.44e3, 1.49e6]; 
-Active2tf = tf(Anum2, Aden2);
+%% 2.2
 
-%% LTI view
-ltiview(Passive1tf);
-ltiview(Passive2tf);
-ltiview(Active1tf);
-ltiview(Active2tf);
+% Define transfer function G1
+numerator_G1 = [1];
+denominator_G1 = [1, 0.5, 1];
+G1 = tf(numerator_G1, denominator_G1);
 
-%%
 % Use lsim to simulate the response of the feedback system
-P1 = lsim(Passive1tf, step_input, t);
-P2 = lsim(Passive2tf, step_input, t);
-A1 = lsim(Active1tf, step_input, t);
-A2 = lsim(Active2tf, step_input, t);
+y1 = lsim(G1, step_input, t2);
 
-%% 3.3
+% Plot the step response
+figure;
+plot(t2, y1);
+title('Step Response of Feedback System');
+xlabel('Time (s)');
+ylabel('Output');
+grid on;
 
+% This isn't good enough because it doesn't rotate all the way.
+
+%% 2.3
+
+% Underdamped
+% Natural frequency (omega-n): 1
+% Damping ratio (zeta): 0.25
+
+% Peak time: 3.24s
+% Settling time: 16s
+% Overshoot %: 44.4%
+
+%% 2.4
+    
+% Define the values for Ggs and Hgs
+Ggs = [0.1, 0.2, 0.5, 1, 2];
+Hgs = [0.1, 0.2, 0.5, 1, 2];
+
+% Loop over the values in Ggs while keeping Hgs constant
+for i = 1:5
+    Ggs_i = 1;
+    num = Ggs_i;
+    den = [1, 0.5, Hgs(i)];
+    G2 = tf(num, den);
+    
+    y = lsim(G2, step_input, t2);
+    
+    subplot(2, 5, i); % Create a subplot grid (2 rows, 5 columns)
+    plot(t2, y);
+    title(['Step Response Kfb = ', num2str(Hgs(i))]);
+    xlabel('Time (s)');
+    ylabel('Output');
+end
+
+% Loop over the values in Hgs while keeping Ggs constant
+for i = 1:5
+    Hgs_i = 1;
+    num2 = Hgs(i);
+    den2 = [1, 0.5, Ggs(i)];
+    G2 = tf(num2, den2);
+    
+    y = lsim(G2, step_input, t2);
+
+    subplot(2, 5, 5+i); % Create a subplot grid (2 rows, 5 columns)
+    plot(t2, y);
+    title(['Step Response Kfwd = ', num2str(Ggs(i))]);
+    xlabel('Time (s)');
+    ylabel('Output');
+end
+sgtitle('System Responses for Different Ggs and Hgs Values');
+
+%% 2.5
+
+% Get the system parameters (2.3) of all the plots created in 2.4, using
+% this determine which gain values (Hg(s) and Gg(s)) is suitable. Requires
+% of the system:
+
+% - Output must accurately travel between 0 to 2pi rads (so preferrably no
+% oscillation).
+% - The camera shouldn't rotate to quickly (peak time shouldn't be too
+% fast, aim for 13 seconds).
+
+% Define transfer function G1
+Fnum = [0.7596]; % 2pi * Wn^2
+Fden = [1, 0.5, 0.1208]; % G(s) General second order transfer function denominator
+cameraTF = tf(Fnum, Fden);
+
+% Use lsim to simulate the response of the feedback system
+y2 = lsim(cameraTF, step_input, t2);
+
+% Plot the step response
+figure;
+plot(t2, y2);
+title('Step Response of Feedback System');
+xlabel('Time (s)');
+ylabel('Output (rad)');
+grid on;
+
+%% 2.6
+% Output = 30 deg = pi/6
+% Output = 210 deg = 7pi/6
+% Input * 2pi = Output ~ Input = Output / 2pi
+
+startVoltage = 1/12;
+endVoltage = 7/12; 
+[startIm, finalIm] = cameraPan(startVoltage, endVoltage, cameraTF);
 %% helper functions
 % function definitions in matlab either need to be in their own file,
 % or can be in at the bottom of a script.
@@ -110,6 +160,28 @@ function t=timevec(t0, t0_plus_T, n)
     t = linspace(t0, t0_plus_T, n + 1);
     t = t(1:end - 1);
 end
+
+
+function fourierTransform = ft(freq, n)
+% Applies complex Fourier transform with the assigned sampling frequency
+%
+%   Args:
+%   freq = frequency/signal input
+%   n = sample frequency in Hz
+
+    fourierTransform = fftshift(fft(freq)) / n;
+end
+
+function inversefourierTransform = ift(freq, n)
+% Applies inverse Fourier transform with the assigned sampling frequency
+%
+%   Args:
+%   freq = frequency/signal input
+%   n = sample frequency in Hz
+
+    inversefourierTransform = ifft(ifftshift(freq)) * n;
+end
+
 
 function f=freqvec(fs, n)
 % Creates frequency vector suitable for plotting magnitude/phase spectrum
@@ -158,25 +230,3 @@ function f=freqvec(fs, n)
         f = linspace(-(n -1)/ 2, (n -1) / 2, n) * fs / n;
     end
 end
-
-function fourierTransform = ft(freq, n)
-% Applies complex Fourier transform with the assigned sampling frequency
-%
-%   Args:
-%   freq = frequency/signal input
-%   n = sample frequency in Hz
-
-    fourierTransform = fftshift(fft(freq)) / n;
-end
-
-function inversefourierTransform = ift(freq, n)
-% Applies inverse Fourier transform with the assigned sampling frequency
-%
-%   Args:
-%   freq = frequency/signal input
-%   n = sample frequency in Hz
-
-    inversefourierTransform = ifft(ifftshift(freq)) * n;
-end
-
-
